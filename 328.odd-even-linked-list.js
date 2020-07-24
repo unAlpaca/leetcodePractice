9d5bb881a5145f40dff89a4c485d9f9a50b6b134
lrg <674931583@qq.com>
2020-07-24 09:51:28 +0800

    leetcode 83,328

---
+++ 328.odd-even-linked-list.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=328 lang=javascript
+ *
+ * [328] Odd Even Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    if(head == null) return null
+    if(head.next == null ) return head
+    let oddPointer = head;
+    let evenPointer = head.next;
+    let evenHead = head.next
+
+    while(evenPointer !== null && evenPointer.next !== null){
+        oddPointer.next = oddPointer.next.next;
+        oddPointer = oddPointer.next
+        evenPointer.next = evenPointer.next.next;
+        evenPointer = evenPointer.next;
+        
+        
+
+    }
+
+    oddPointer.next = evenHead
+    return head
+};
+// @lc code=end
+
